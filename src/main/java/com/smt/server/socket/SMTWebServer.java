6f6a3816d30a1467bba829a5df8cea5b21d05c09
smt-etewa <james@siliconmtn.com>
2021-07-06 12:51:13 -0600

    feat(init): startiung a new kata project for the threaded server

---
+++ src/main/java/com/smt/server/socket/SMTWebServer.java
@@ -0,0 +1,29 @@
+package com.smt.server.socket;
+
+// Lombok 1.x
+import lombok.extern.log4j.Log4j2;
+
+/****************************************************************************
+ * <b>Title</b>: SMTWebServer.java
+ * <b>Project</b>: Daily-SocketServer-Kata
+ * <b>Description: </b> Socket Server Kata
+ * This class is the main entry point into your server
+ * 
+ * <b>Copyright:</b> Copyright (c) 2021
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author James Camire
+ * @version 3.0
+ * @since Jun 28, 2021
+ * @updates:
+ ****************************************************************************/
+@Log4j2
+public class SMTWebServer {
+	/**
+	 * Kicks off the threaded server
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		log.info("Do Something Here");
+	}
+}
